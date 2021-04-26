c9ed567df1bd132e46afa0a57b1b1d2487151da4
jaeyoon <yjy2020@kyungnam.ac.kr>
2021-04-26 14:37:53 +0900

    v0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# plantpulse-plugin-aas-server
+
+플랜트펄스 플러그인 - AAS(AssetAdministrationShel) 서버
---
+++ src/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+import plantpulse.plugin.aas.Main;
+
+/**
+ * Test
+ * @author leesa
+ *
+ */
+public class Test {
+
+	public static void main(String[] args){
+		Main.main(new String[] {
+			"C:\\Users\\leesa\\git\\plantpulse-plugin-aas-server\\config\\aas.server.config.json"
+		});
+	};
+
+}
